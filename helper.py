45e505f089d6ef1fada9031f093eb60426696d3d
AnthonyTnns <teciorowskianthony3@gmail.com>
2023-12-15 18:15:58 -0800

    more funcs

--- helper.py
+++ helper.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, when
 
 def get_status_counts(df):
     status_list = df.select("status").distinct().rdd.flatMap(lambda x: x).collect()
@@ -8,4 +8,28 @@ def get_status_counts(df):
         count = df_status.count()
         print(f"Status: {status}, Count: {count}")
 
+def get_successful_jobs(df):
+    return df.filter(col("status") == "Successful")
+
+def get_failed_jobs(df):
+    return df.filter(col("status") == "Failed")
+
+def get_pending_jobs(df):
+    return df.filter(col("status") == "Pending")
+
+def get_in_progress_jobs(df):
+    return df.filter(col("status") == "In Progress")
+
+def count_pending_in_progress_jobs(df):
+    count = df.filter(
+        (col("status").isin("Pending", "In Progress"))
+    ).count()
+
+    return count
+
 def retry_failed_jobs(failed_df): 
+    return failed_df.withColumn("status", when(col("status") == "Failed", "Pending").otherwise(col("status")))
+
+def filter_jobs_by_team(df, team_name):
+    filtered_df = df.filter(df.teamName == team_name)
+    return filtered_df
